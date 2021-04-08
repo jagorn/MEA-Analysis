31f92b93859075e6075c2b0e6f645b7368a3fd6e
FranTr <jagorn90@gmail.com>
2021-04-08 18:16:15 +0200

    flash_stim_added

---
+++ Code/Repetitions/maps/createFlashMap.m
@@ -0,0 +1,30 @@
+function createFlashMap()
+% Creates the repetition map for the flash stimululs.
+% The repetition map is saved in the stimuli folder.
+
+% repetitions_map: a cell array describing the structure of the stimulus.
+%   repetitions_map.stim_duration: the whole duration (in frames) of the stimulus
+%   repetitions_map.names{i}: the name of the i-th repeated patterns of the stimulus
+%   repetitions_map.start_indexes{i}: an array representing the indexes of the starting frame of the i-th repeated pattern
+%   repetitions_map.durations{i}: the duration (in frames) of the i-th repeated pattern
+
+flash_version = 'flash1_ON_4_OFF';
+
+stim_duration = 30;
+rep_begin_white = 1 : 5 : stim_duration;
+rep_begin_black = 2 : 5 : stim_duration;
+
+
+repetitions_map.names{1} = 'flash_on';
+repetitions_map.names{2} = 'flash_off';
+
+repetitions_map.start_indexes{1} = rep_begin_white;
+repetitions_map.start_indexes{2} = rep_begin_black;
+
+repetitions_map.durations{1} = 1;
+repetitions_map.durations{2} = 4;
+
+repetitions_map.stim_duration = stim_duration;
+
+stim_file = fullfile(stimPath('flash'), strcat(lower(flash_version), '.mat'));
+save(stim_file, 'repetitions_map');
