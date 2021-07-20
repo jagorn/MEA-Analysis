86fff86d9c491ff6dbb95b4ca37be8e03d50cb5b
jagorn <jagorn90@gmail.com>
2021-07-20 18:10:46 +0200

    polarity

--- Code/STAs/Polarity/getPolarity.m
+++ Code/STAs/Polarity/getPolarity.m
@@ -1,8 +1,15 @@
-function polarities = getPolarity(temporalSTAs)
+function polarities = getPolarity(temporalSTAs, baseline_bins)
+
+if ~exist('baseline_bins', 'var')
+    baseline_bins = 1:5;
+end
+
+baseline = mean(temporalSTAs(:, baseline_bins), 2);
 
 n_cells = size(temporalSTAs, 1);
 for i_cell = 1:n_cells
-    if   abs(max(temporalSTAs(i_cell, :))) > abs(min(temporalSTAs(i_cell, :)))
+    norm_sta = temporalSTAs(i_cell, :) - baseline(i_cell);
+    if   abs(max(norm_sta)) > abs(min(norm_sta))
         polarities(i_cell) = "ON";
     else
         polarities(i_cell) = "OFF";
