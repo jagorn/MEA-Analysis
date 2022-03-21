9fc15408fef47680d9573b3eff7b9045bf4908ad
jagorn <jagorn90@gmail.com>
2022-03-21 18:20:43 +0100

    removed-articles-stuff

--- Articles/Typing/toCalciumNL.m
+++
@@ -1,6 +0,0 @@
-function calcium_trace = toCalciumNL(time_sequence, spike_train)
-
-load('calcium_conversion.mat', 'calcium_nl_polynome');
-
-calcium_trace_linear = toCalciumLinear(time_sequence, spike_train);
-calcium_trace = polyval(calcium_nl_polynome, calcium_trace_linear);
