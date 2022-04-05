eb1dce5e54df7b088bdadeb56bd720c8abf26b65
jagorn <jagorn90@gmail.com>
2022-04-05 19:14:46 +0200

    strych2

--- Articles/Strychnine/Code/doAllAnalyses.m
+++ Articles/Strychnine/Code/doAllAnalyses.m
@@ -2,30 +2,58 @@ clear
 close all
 load('strychnine');
 
+% Parameters PSTH
 dt_psth = 0.05;
+
+% Parameters Activation
 ctrl_win = [0, 1];
 resp_win = [1.5, 2.5];
 k = 5;
 min_fr = 10;
 
+% Parameters Registration
+check_size_pxl = 30;
+pxl_size_microns = 3.5;
+surround_edge = 19;
+
+% Variables
+n_cells = numel(cellsTable);
+check_size_microns = check_size_pxl * pxl_size_microns;
+
+rfs(n_cells) = polyshape();
+distance_surround = zeros(1, n_cells);
+
 % Polarity
 polarities = getPolarity(sta_temporal, 1:10);
-for i_p = 1:numel(polarities)
+for i_p = 1:n_cells
     cellsTable(i_p).polarity = polarities(i_p);
 end
 
-on_idx = find(polarities == "ON");
-off_idx = find(polarities == "OFF");
-onoff_idx = find(polarities == "ON-OFF");
-none_idx = find(polarities == "NONE");
+for i_cell = 1:n_cells
+    
+    % Receptive Fields
+    frame_sta = squeeze(sta_spatial(i_cell, :, :));
+    [xEll, yEll, ~, ~] =  fitEllipse(frame_sta);
+    rfs(i_cell) = polyshape(xEll, yEll);
+    
+    % Distance
+    lowest = max(yEll);
+    distance_checks = surround_edge - lowest;
+    distance_microns = distance_checks * check_size_microns;
+    distance_surround(i_cell) = distance_microns;
+end
 
-% Distance 
 
 % Activation
 [z_control, t_up_control, t_down_control] = estimateZscore2sides(psth_surround_control, t_surround_psth, dt_psth, ctrl_win, resp_win, k, min_fr);
 [z_strychnine, t_up_strychnine, t_down_strychnine] = estimateZscore2sides(psth_surround_strychnine, t_surround_psth, dt_psth, ctrl_win, resp_win, k, min_fr);
 
-% Plots
+activations.control.z = z_control;
+activations.control.t_up = t_up_control;
+activations.control.t_udown = t_down_control;
+activations.strychnine.z = z_strychnine;
+activations.strychnine.t_up = t_up_strychnine;
+activations.strychnine.t_udown = t_down_strychnine;
 
 % Save
-save('strychnine', 'cellsTable', '-append');
+save('strychnine', 'cellsTable', 'rfs', 'activations', 'distance_surround', '-append');
--- Articles/Strychnine/Code/printData.m
+++ Articles/Strychnine/Code/printData.m
@@ -1,17 +1,67 @@
 clear
-% close all
+close all
 clc
 
+% Load
 load('strychnine');
 load('Data/euler_stim.mat', 'chirp_stim', 'chirp_stim_time');
 chirp_stim = chirp_stim - min(chirp_stim(:));
 chirp_stim = chirp_stim / max(chirp_stim(:)) * 30 + 10;
 
-n_cells = numel(cellsTable);
+% Choose cells
+off_cells = [cellsTable.polarity] == "OFF";
+on_cells = [cellsTable.polarity] == "ON";
+surround_cells = distance_surround > 0;
+surround_activated = activations.control.z > 0;
+strychnine_activated = activations.strychnine.z > 0;
+
+cell_idx = find(off_cells & surround_cells & (surround_activated | strychnine_activated));
+% cell_idx = find(strychnine_activated);
+
+% Recap Plot
+% figure();
+% 
+% subplot(1, 3, 3);
+% hold on
+% plot([-25, 50], [-25, 50], '--', 'Color', [0, 0, 0, 0.5]);
+% scatter(activations.control.z(cell_idx), activations.strychnine.z(cell_idx), 30, 'b', 'Filled', 'MarkerFaceAlpha', 0.5, 'MarkerEdgeAlpha', 0);
+% xlabel('surr. resp. control [\delta fr]');
+% ylabel('surr. resp. strychnine [\delta fr]');
+% daspect([1, 1, 1]);
+% ylim([-25, 50]);
+% xlim([-25, 50]);
+% 
+% all_data = [activations.control.z(cell_idx) activations.strychnine.z(cell_idx)];
+% edge_min = min(all_data);
+% edge_max = max(all_data);
+% edges = linspace(edge_min, edge_max, 20);
+% 
+% subplot(1, 3, 1);
+% histogram(activations.control.z(cell_idx), edges, 'Normalization','probability');
+% ylim([0, 1]);
+% ylabel('probability');
+% xlabel('surround response [\delta fr]');
+% title('Control');
+% 
+% subplot(1, 3, 2);
+% histogram(activations.strychnine.z(cell_idx), edges, 'Normalization','probability');
+% ylim([0, 1]);
+% ylabel('probability');
+% xlabel('surround response [\delta fr]');
+% title('Strychnine');
+% 
+% suptitle(strcat("Surround-Activated OFF RGCs (size ", num2str(numel(cell_idx)), ")"));
+% waitforbuttonpress();
+
+% Figures
 figure();
 fullScreen();
 
-for i_cell = 1:n_cells
+for i_cell = cell_idx
+    
+    if isequal(cellsTable(i_cell).polarity, 'NONE')
+        continue;
+    end
     
     subplot(2, 3, 1);
     area(t_isi, isi(i_cell, :));
@@ -36,6 +86,7 @@ for i_cell = 1:n_cells
     imagesc(norm_spatial);
     axis off
     hold on;
+    plot(rfs(i_cell));
     rectangle('Position', [0.5, 18.5, 24, 6], 'FaceColor', [1, 1, 1, 0.6], 'EdgeColor', 'white');
     daspect([1, 1, 1]);
     title('spatial STA');
@@ -50,9 +101,14 @@ for i_cell = 1:n_cells
     
     subplot(2, 3, 6);
     hold on;
+    
     p0 = area([1.5 2.5], [100 100], 'FaceColor', 'yellow', 'FaceAlpha', 0.3, 'EdgeColor', 'None');
     p1 = plot(t_surround_psth, psth_surround_control(i_cell, :), 'k');
     p2 = plot(t_surround_psth, psth_surround_strychnine(i_cell, :), 'r');
+    
+    yline(activations.control.t_up(i_cell), 'k--');
+    yline(activations.strychnine.t_up(i_cell), 'r--');
+    
     legend([p0, p1, p2], ["stimulus", "psth control", "psth strychnine"]);
     ylim([0, 50]);
     xlabel('time [s]');
@@ -61,6 +117,5 @@ for i_cell = 1:n_cells
     
     suptitle(strcat("Cell #", num2str(cellsTable(i_cell).N), ", experiment: ", cellsTable(i_cell).experiment, ", polarity ", cellsTable(i_cell).polarity, ", grade = ", cellsTable(i_cell).grade));
     
-    waitforbuttonpress();
-    %     close;
+    export_fig(strcat('Plots/cards/StrychnineCard_Cell#', num2str(cellsTable(i_cell).N), '.png'));
 end
